30cea4cd91e504f805b6b2efb69a9aca937c6846
Sooraj M Singh <sooraj.singh@leafround.com>
2024-01-04 19:09:14 +0530

    content and images

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,66 +8,62 @@ import { SpeedInsights } from '@vercel/speed-insights/next';
 import { PreloadResources } from './preload';
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://leerob.io'),
-  title: {
-    default: 'Lee Robinson',
-    template: '%s | Lee Robinson',
-  },
-  description: 'Developer, writer, and creator.',
-  openGraph: {
-    title: 'Lee Robinson',
-    description: 'Developer, writer, and creator.',
-    url: 'https://leerob.io',
-    siteName: 'Lee Robinson',
-    locale: 'en_US',
-    type: 'website',
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      index: true,
-      follow: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
-    },
-  },
-  twitter: {
-    title: 'Lee Robinson',
-    card: 'summary_large_image',
-  },
-  verification: {
-    google: 'eZSdmzAXlLkKhNJzfgwDqWORghxnJ8qR9_CHdAh5-xw',
-    yandex: '14d2e73487fa6c71',
-  },
+	metadataBase: new URL('https://thesoorajsingh.me'),
+	title: {
+		default: 'Sooraj Singh',
+		template: '%s | Sooraj Singh',
+	},
+	description: 'Engineer, Writer and an Optimist',
+	openGraph: {
+		title: 'Sooraj Singh',
+		description: 'Engineer, Writer and an Optimist.',
+		url: 'https://thesoorajsingh.me',
+		siteName: 'Sooraj Singh',
+		locale: 'en_US',
+		type: 'website',
+	},
+	robots: {
+		index: true,
+		follow: true,
+		googleBot: {
+			index: true,
+			follow: true,
+			'max-video-preview': -1,
+			'max-image-preview': 'large',
+			'max-snippet': -1,
+		},
+	},
+	twitter: {
+		title: 'Sooraj Singh',
+		card: 'summary_large_image',
+	},
 };
 
 const cx = (...classes) => classes.filter(Boolean).join(' ');
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html
-      lang="en"
-      className={cx(
-        'text-black bg-white dark:text-white dark:bg-[#111010]',
-        GeistSans.variable,
-        GeistMono.variable
-      )}
-    >
-      <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
-        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
-          <Navbar />
-          {children}
-          <Analytics />
-          <SpeedInsights />
-          <PreloadResources />
-        </main>
-      </body>
-    </html>
-  );
+	return (
+		<html
+			lang='en'
+			className={cx(
+				'text-black bg-white dark:text-white dark:bg-[#111010]',
+				GeistSans.variable,
+				GeistMono.variable
+			)}
+		>
+			<body className='antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto'>
+				<main className='flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0'>
+					<Navbar />
+					{children}
+					<Analytics />
+					<SpeedInsights />
+					<PreloadResources />
+				</main>
+			</body>
+		</html>
+	);
 }
