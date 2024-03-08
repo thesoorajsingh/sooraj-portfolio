d115f50a4f4ec6e80aa1ca50811e6c43cc97645f
Sooraj M Singh <sooraj.singh@leafround.com>
2024-03-08 12:19:37 +0530

    [feature] added blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,3 +1,4 @@
+import { getBlogPosts } from 'app/db/blog';
 import Link from 'next/link';
 
 export const metadata = {
@@ -6,23 +7,51 @@ export const metadata = {
 };
 
 export default function BlogPage() {
+	let blogs = getBlogPosts();
+
 	return (
 		<section>
-			<h1 className='font-medium text-2xl mb-8 tracking-tighter'>
-				i'm still working on this part.
-				<br /> sign up on my{' '}
+			{blogs
+				.sort((a, b) => {
+					if (
+						new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)
+					) {
+						return -1;
+					}
+					return 1;
+				})
+				.map((post) => (
+					<>
+						<Link
+							key={post.slug}
+							className='flex flex-col space-y-1 mb-4'
+							href={`/blog/${post.slug}`}
+						>
+							<div className='w-full flex flex-row'>
+								<p className={'pr-2'}>{'->'}</p>
+								<p className='text-neutral-900 dark:text-neutral-100 tracking-tight hover:underline transition-all duration-300 ease-in-out'>
+									{post.metadata.title}
+								</p>
+							</div>
+							<p className='text-neutral-500 dark:text-neutral-400'>
+								{'Substack'}
+							</p>
+						</Link>
+					</>
+				))}
+			<hr className={`text-neutral-400`} />
+			<p className={'mt-4'}>
+				i write every week about something that helped me grow. follow along on{' '}
 				<Link
+					className={
+						'underline hover:text-orange-400 transition-all duration-300'
+					}
 					href={'https://soorajsingh.substack.com'}
 					target='_blank'
-					rel='noopener noreferrer'
-					className={
-						'underline hover:text-orange-600 dark:hover:text-orange-400'
-					}
 				>
-					substack
-				</Link>{' '}
-				to get updates on when I post new blogs!
-			</h1>
+					{'my substack'}
+				</Link>
+			</p>
 		</section>
 	);
 }
