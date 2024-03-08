e00310d60290163d0b91c4133c83218b619278b8
Sooraj M Singh <sooraj.singh@leafround.com>
2024-03-08 13:22:59 +0530

    [wip] remove font import

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -6,10 +6,10 @@ export const runtime = 'edge';
 export async function GET(req: NextRequest) {
 	const { searchParams } = req.nextUrl;
 	const postTitle = searchParams.get('title');
-	const font = fetch(
-		new URL('../../public/fonts/kaisei-tokumin-bold.ttf', import.meta.url)
-	).then((res) => res.arrayBuffer());
-	const fontData = await font;
+	// const font = fetch(
+	// 	new URL('../../public/fonts/kaisei-tokumin-bold.ttf', import.meta.url)
+	// ).then((res) => res.arrayBuffer());
+	// const fontData = await font;
 
 	return new ImageResponse(
 		(
