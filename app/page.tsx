44f0dd0ff01796e7f66c0e6c31cec92494e54dbe
Sooraj M Singh <soorajsingh1999@gmail.com>
2024-05-27 20:14:47 +0530

    fix: mobile

--- app/page.tsx
+++ app/page.tsx
@@ -106,7 +106,7 @@ async function Views({ slug }: { slug: string }) {
 export default function Page() {
 	return (
 		<section>
-			<h1 className='font-medium text-2xl mb-8 leading-16 font-["monospace"] flex items-center gap-1'>
+			<h1 className='font-medium text-2xl mb-8 leading-16 font-["monospace"] md:flex items-center gap-1'>
 				sup, i'm sooraj 👋 and i'm{' '}
 				<Link
 					href={'https://read.cv/thesoorajsingh'}
@@ -116,8 +116,8 @@ export default function Page() {
 					{' '}
 					open to work!
 				</Link>{' '}
-				<span className='relative flex h-3 w-3 -top-2 -right-1'>
-					<span className='animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75'></span>
+				<span className='relative h-3 w-3 md:flex xs:flex -top-2 -right-1'>
+					<span className='animate-ping absolute inline-flex md:h-full md:w-full rounded-full bg-green-400 opacity-75'></span>
 					<span className='relative inline-flex rounded-full h-3 w-3 bg-green-500'></span>
 				</span>
 			</h1>
